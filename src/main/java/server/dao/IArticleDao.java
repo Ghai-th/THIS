793c961fdc410b6fb69ea976dc0d5a1a0cc3e61f
auto <auto>
2021-01-02 11:21:37 +0800

    db

--- src/main/java/server/dao/IArticleDao.java
+++ src/main/java/server/dao/IArticleDao.java
@@ -4,6 +4,20 @@ import client.entity.Article;
 
 public interface IArticleDao {
 
-    int addArticle(Article article);
+    /**
+     * 文章对象加入数据库
+     * @param article 文章
+     * @return 返回结果
+     */
+    boolean addArticle(Article article);
+
+    /**
+     * 根据id删除文章
+     * @param aid id
+     * @return 返回删除结果
+     */
+    boolean deleteArticle(String aid);
+
+
 
 }
--- src/main/java/server/dao/impl/ArticleDaoImpl.java
+++ src/main/java/server/dao/impl/ArticleDaoImpl.java
@@ -1,6 +1,53 @@
 package server.dao.impl;
 
+import client.entity.Article;
 import server.dao.IArticleDao;
+import server.util.DBUtil;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 
 public class ArticleDaoImpl implements IArticleDao {
+
+    private Statement statement = null;
+    private Connection connection = null;
+    private PreparedStatement preparedStatement = null;
+    @Override
+    public boolean addArticle(Article article) {
+        int num = 0;
+        try {
+            String sql = "insert into article values(?,?,?,?,?,?,?,?,?,?,?,?)";
+            connection = DBUtil.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,article.getAid());
+            preparedStatement.setString(2,article.getUid());
+            preparedStatement.setString(3,article.getCid());
+            preparedStatement.setString(4,article.getTitle());
+            preparedStatement.setString(5,article.getSynopsis());
+            preparedStatement.setString(6,article.getText());
+            preparedStatement.setString(7,
+                    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(article.getCreate()));
+            preparedStatement.setString(8,
+                    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(article.getUpdate()));
+            preparedStatement.setInt(9,article.getVisitorNum());
+            preparedStatement.setInt(10,article.getLikeNum());
+            preparedStatement.setInt(11,article.getCollectNum());
+            preparedStatement.setBlob(12,article.getImage());
+            num = preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.closeResources(connection,preparedStatement);
+            return num != 0;
+        }
+    }
+
+    @Override
+    public boolean deleteArticle(String aid) {
+        return false;
+    }
 }
