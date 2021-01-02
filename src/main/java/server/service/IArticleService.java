1f9adbdd56a1e6ec999870298f6d84a4460d7ba9
auto <auto>
2021-01-02 15:57:09 +0800

    ArticleServiceImpl

--- src/main/java/server/service/IArticleService.java
+++ src/main/java/server/service/IArticleService.java
@@ -1,4 +1,63 @@
 package server.service;
 
+import client.entity.Article;
+
+import java.util.List;
+
 public interface IArticleService {
+    /**
+     * 文章对象加入数据库
+     * @param article 文章
+     * @return 返回结果
+     */
+    boolean addArticle(Article article);
+
+    /**
+     * 根据id删除文章
+     * @param aid id
+     * @return 返回删除结果
+     */
+    boolean deleteArticle(String aid);
+
+    /**
+     * 修改文章信息
+     * @param article 文章对象
+     * @return 修改结果
+     * 修改文章的更新时间，各种num
+     */
+    boolean updateArticle(Article article);
+
+    /**
+     * 根据aid查询文章
+     * @param aid
+     * @return
+     */
+    Article selectArticleByAid(String aid);
+
+    /**
+     * 根据分类号查找文章
+     * @param cid 分类编号
+     * @return 返回所有符号条件的文章的list
+     */
+    List<Article> selectArticleByCid(String cid);
+
+    /**
+     * 根据文章标题模糊查询文章列表
+     * @param tittle 文章的标题
+     * @return 返回文章列表
+     */
+    List<Article> selectArticleByTittle(String tittle);
+
+    /**
+     * 根据作者id查找文章
+     * @param uid 作者id
+     * @return 返回文章列表
+     */
+    List<Article> selectArticleByUid(String uid);
+
+    /**
+     * 查询前10的文章
+     * @return 文章列表
+     */
+    List<Article> selectTopTenArticle();
 }
--- src/main/java/server/service/impl/ArticleServiceImpl.java
+++ src/main/java/server/service/impl/ArticleServiceImpl.java
@@ -1,6 +1,52 @@
 package server.service.impl;
 
+import client.entity.Article;
+import server.dao.IArticleDao;
+import server.dao.impl.ArticleDaoImpl;
 import server.service.IArticleService;
 
+import java.util.List;
+
 public class ArticleServiceImpl implements IArticleService {
+    private IArticleDao articleDao = new ArticleDaoImpl();
+
+    @Override
+    public boolean addArticle(Article article) {
+        return articleDao.addArticle(article);
+    }
+
+    @Override
+    public boolean deleteArticle(String aid) {
+        return articleDao.deleteArticle(aid);
+    }
+
+    @Override
+    public boolean updateArticle(Article article) {
+        return articleDao.updateArticle(article);
+    }
+
+    @Override
+    public Article selectArticleByAid(String aid) {
+        return articleDao.selectArticleByAid(aid);
+    }
+
+    @Override
+    public List<Article> selectArticleByCid(String cid) {
+        return articleDao.selectArticleByCid(cid);
+    }
+
+    @Override
+    public List<Article> selectArticleByTittle(String tittle) {
+        return articleDao.selectArticleByTittle(tittle);
+    }
+
+    @Override
+    public List<Article> selectArticleByUid(String uid) {
+        return articleDao.selectArticleByUid(uid);
+    }
+
+    @Override
+    public List<Article> selectTopTenArticle() {
+        return articleDao.selectTopTenArticle();
+    }
 }
