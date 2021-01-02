f13b0b49ab7f45ee499aac30c1b67e80b9441e8a
Lked <1220606578@qq.com>
2021-01-02 11:29:17 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/server/dao/IArticleDao.java

--- src/main/java/server/dao/IUserDao.java
+++ src/main/java/server/dao/IUserDao.java
@@ -1,4 +1,26 @@
 package server.dao;
 
+import client.entity.User;
+
+import java.util.Date;
+
 public interface IUserDao {
+    void addUser(User user);
+
+    void deleteUser(String uid);
+
+    void updateUserName(String name);
+    void updateUserPassword(String password);
+    void updateUserGender(Integer gender);
+    void updateUserImage(Byte[]image);
+    void updateUserFansNum(Integer fansNum);
+    void updateUserAttentionnum(Integer attentionNum);
+    void updateUserVisitorNum(Integer visitorNum);
+    void updateUserArticleNum(Integer articleNum);
+    void updateUserCreate(Date create);
+    void updateUserUpdate(Date update);
+    void updateUserSynopsis(String synopsis);
+    void updateUserActive(Integer active);
+
+    void selectUser(User user);
 }
