a739bbbbfb8f9701ea281d097e286cc2e3561d71
auto <auto>
2021-01-03 17:41:46 +0800

    commment

---
+++ src/main/java/server/service/impl/AttentionServiceImpl.java
@@ -0,0 +1,21 @@
+package server.service.impl;
+
+import client.entity.Attention;
+import server.service.IAttentionService;
+
+public class AttentionServiceImpl implements IAttentionService {
+    @Override
+    public boolean addAttention(Attention attention) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAttention(Attention attention) {
+        return false;
+    }
+
+    @Override
+    public boolean searchAttention(Attention attention) {
+        return false;
+    }
+}
