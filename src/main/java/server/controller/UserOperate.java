f1ef8f088220c59df5b12cb6ce90ea785f297624
Lked <1220606578@qq.com>
2021-01-03 17:33:54 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/server/dao/IArticleDao.java

--- src/main/java/server/controller/UserOperate.java
+++ src/main/java/server/controller/UserOperate.java
@@ -3,32 +3,21 @@ package server.controller;
 import client.entity.Comment;
 import client.entity.User;
 import data.Operate;
+import server.service.IUserService;
 import server.service.impl.CommentServiceImpl;
 import server.service.impl.UserServiceImpl;
 
 public class UserOperate {
-    private Operate userOperate;
     private User user;
-    public UserServiceImpl userServiceImpl;
-    public UserOperate(Operate userOperate) {
-        this.userOperate = userOperate;
+    public IUserService userServiceImpl = new UserServiceImpl();
+
+    public UserOperate(User user) {
+        this.user = user;
     }
 
-    public Operate getUserOperate() {
-        return userOperate;
+    public UserOperate(){
+
     }
 
-    public void setUserOperate(Operate userOperate) {
-        this.userOperate = userOperate;
-    }
-
-    public void executeUserOperate() {
-        switch(userOperate.operate) {
-            case ServerOperate.REGISTER_USER :
-                //userServiceImpl.register();
-                break;
-
-        }
-    }
 
 }
