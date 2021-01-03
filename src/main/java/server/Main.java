2d9aa37750ac4ec5a16fc5c765fa28e5f9831e42
Lenovo <1208651874@qq.com>
2021-01-03 08:58:27 +0800

    2

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,4 +1,9 @@
 package server;
 
+import server.controller.ServerOperate;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(ServerOperate.DELETE_COMMENT_BY_AID);
+    }
 }
--- src/main/java/server/controller/ServerOperate.java
+++ src/main/java/server/controller/ServerOperate.java
@@ -4,6 +4,6 @@ package server.controller;
  * 0到99999范围内定义不同的操作
  * 所有操作命名全部大写字母加下划线
  */
-public interface ServerOperate {
-
-}
+public enum ServerOperate {
+    ADD_COMMENT, DELETE_COMMENT, DELETE_COMMENT_BY_UID, DELETE_COMMENT_BY_AID, UPDATE_COMMENT, QUERY_ALL_COMMENT_BY_UID, QUERY_ALL_COMMENT_BY_AID
+};
