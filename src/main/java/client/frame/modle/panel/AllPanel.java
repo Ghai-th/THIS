6a6f2f0c5c3ed5a1c4dc9f241004a8ba5bc01ecc
auto <auto>
2021-01-05 19:16:47 +0800

    copy

--- src/main/java/client/frame/modle/panel/AllPanel.java
+++ src/main/java/client/frame/modle/panel/AllPanel.java
@@ -29,7 +29,7 @@ public class AllPanel extends JPanel{
         this.commentList = commentList;
         this.storeList = storeList;
         this.articleList = articleList;
-        this.myUser = User.copyUser(myUser);
+        this.myUser = myUser;
         this.otherUser = otherUser;
         this.index = index;
         init();
