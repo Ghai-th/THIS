a4bb309c79f374b03fd5792bdbaf615934fd891e
Lked <1220606578@qq.com>
2021-01-05 21:35:31 +0800

    666

--- src/main/java/server/controller/UserOperate.java
+++ src/main/java/server/controller/UserOperate.java
@@ -263,6 +263,7 @@ public class UserOperate {
         }
         if(success){
             try {
+                user.setOperate(ServerOperate.SUCCESS);
                 serverUtil.sendInfo(user,User.class);
             } catch (IOException e) {
                 e.printStackTrace();
