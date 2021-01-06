fb72be183dedff320f8bfb43b7279ec48131d847
Lenovo <1208651874@qq.com>
2021-01-06 22:14:36 +0800

    2

--- src/main/java/client/frame/Administrate.java
+++ src/main/java/client/frame/Administrate.java
@@ -175,6 +175,7 @@ public class Administrate extends JPanel implements IndexConf {
 
         functionJPanel.add(deleteUser);
         functionJPanel.setPreferredSize(new Dimension(WIDTH / 6, HIGH * 53 / 1050));
+
         /**
          * 用户事件对应的适配器
          */
@@ -213,7 +214,7 @@ public class Administrate extends JPanel implements IndexConf {
         deleteUser.addMouseListener(userAdepter);
 
         /**
-        * 文章事件的监听器
+        * 文章事件的适配器
         */
         articleAdepter = new MouseAdapter() {
             @Override
@@ -247,6 +248,10 @@ public class Administrate extends JPanel implements IndexConf {
             }
         };
         deleteArticle.addMouseListener(articleAdepter);
+
+        /**
+         * 评论的事件适配器
+         */
         commentAdapter = new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -280,6 +285,10 @@ public class Administrate extends JPanel implements IndexConf {
                 row = -1;
             }
         };
+
+        /**
+         *
+         */
         deleteComment.addMouseListener(commentAdapter);
         reportDetail.addMouseListener(new MouseAdapter() {
             @Override
