8687ca3f547d26758315f7e8085fcf591b917e70
王淑花 <1846199122@qq.com>
2021-01-07 10:08:35 +0800

    tip

--- src/main/java/server/dao/IStoreDao.java
+++ src/main/java/server/dao/IStoreDao.java
@@ -5,7 +5,24 @@ import client.entity.Store;
 import java.util.List;
 
 public interface IStoreDao {
+    /**
+     * 添加收藏
+     * @param store
+     * @return返回添加结果
+     */
     boolean addStore(Store store);
+
+    /**
+     * 删除收藏
+     * @param store
+     * @return返回删除结果
+     */
     boolean deleteStore(Store store);
+
+    /**
+     * 查询收藏信息
+     * @param store
+     * @return返回信息结果
+     */
     List<Store> selectStore(Store store);
 }
